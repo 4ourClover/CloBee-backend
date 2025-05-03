38490334814f24a37ed477b239da9385ba479849
ssoyeonni <thduswja23@gmail.com>
2025-05-03 23:44:50 +0900

    fix : 카드 신청하기 수정

--- src/main/java/com/fourclover/clobee/card/controller/CardController.java
+++ src/main/java/com/fourclover/clobee/card/controller/CardController.java
@@ -6,11 +6,9 @@ import com.fourclover.clobee.card.domain.CardPageDTO;
 import com.fourclover.clobee.card.domain.UserCardDetail;
 import com.fourclover.clobee.card.service.CardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -35,14 +33,21 @@ public class CardController {
         return cardService.getCardBenefitDetail(cardInfoId);
     }
 
-    // 카드 신청하기
-    // http://localhost:8080/api/card/apply?cardInfoId=1470&cardBrand=302
+//    // 카드 신청하기
+//    // http://localhost:8080/api/card/apply?cardInfoId=1470&cardBrand=302
+//    @GetMapping("/apply")
+//    public ResponseEntity<Void> applyCard(@RequestParam Long cardInfoId, @RequestParam int cardBrand) {
+//        String url = cardService.getCardBrandUrlAndIncreaseApplyViews(cardInfoId, cardBrand);
+//        return ResponseEntity.status(HttpStatus.FOUND)  // HttpStatus.FOUND는 302 Redirect를 의미
+//                .location(URI.create(url))
+//                .build();
+//    }
+
+    // 카드 신청하기 URL 반환 (리다이렉트 대신 문자열 반환)
     @GetMapping("/apply")
-    public ResponseEntity<Void> applyCard(@RequestParam Long cardInfoId, @RequestParam int cardBrand) {
+    public ResponseEntity<String> applyCard(@RequestParam Long cardInfoId, @RequestParam int cardBrand) {
         String url = cardService.getCardBrandUrlAndIncreaseApplyViews(cardInfoId, cardBrand);
-        return ResponseEntity.status(HttpStatus.FOUND)  // HttpStatus.FOUND는 302 Redirect를 의미
-                .location(URI.create(url))
-                .build();
+        return ResponseEntity.ok(url);
     }
 
     // 내 카드 추가하기 - id를 저장하는 방식에 따라 수정됨
